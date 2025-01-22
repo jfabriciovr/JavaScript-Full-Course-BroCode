3913659d1c4e3a767de93021c54f109eb73f24cc
jfabriciovr <46113391+jfabriciovr@users.noreply.github.com>
2025-01-21 20:27:50 -0600

    update random password generator program

--- 10-random-password-generator/index.js
+++ 10-random-password-generator/index.js
@@ -1,7 +1,10 @@
 // RANDOM PASSWORD GENERATOR
 
+const generateBtn = document.getElementById("generateBtn");
+const passwordDisplay = document.getElementById("passwordDisplay");
+
 function generatePassword(
-  length,
+  passwordLength,
   includeLowercase,
   includeUppercase,
   includeNumbers,
@@ -15,21 +18,42 @@ function generatePassword(
   let allowedChars = "";
   let password = "";
 
-  return "";
+  // evaluamos si vamos a incluir los distintos grupos de letras, números o símbolos
+  // pretuntamos, es includeLowecase true? si es así,
+  // allowedChars += lowercaseChars
+  // si no, se le suma una cadena vacía, o sea, se omite
+  //luego preguntamos lo mismo para cada caso
+  allowedChars += includeLowercase ? lowercaseChars : "";
+  allowedChars += includeUppercase ? uppercaseChars : "";
+  allowedChars += includeNumbers ? numberChars : "";
+  allowedChars += includeSymbols ? symbolChars : "";
+  // al final de estas evaluaciones, quedará un string concatenado
+  // de los símbolos autorizados
+
+  // console.log(allowedChars);
+  // abcdefghijlknopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!$%&/()=+-
+
+  for (let i = 0; i < passwordLength; i++) {
+    const randomIndex = Math.floor(Math.random() * allowedChars.length);
+    password += allowedChars[randomIndex];
+  }
+  return password;
 }
 
-const passwordLength = 12;
-const includeLowercase = true;
-const includeUppercase = true;
-const includeNumbers = true;
-const includeSymbols = true;
+generateBtn.onclick = function () {
+  const passwordLength = 12;
+  const includeLowercase = true;
+  const includeUppercase = true;
+  const includeNumbers = true;
+  const includeSymbols = true;
 
-const password = generatePassword(
-  passwordLength,
-  includeLowercase,
-  includeUppercase,
-  includeNumbers,
-  includeSymbols
-);
-
-console.log(`Generated password: ${password}`);
+  const password = generatePassword(
+    passwordLength,
+    includeLowercase,
+    includeUppercase,
+    includeNumbers,
+    includeSymbols
+  );
+  //console.log(`Generated password: ${password}`);
+  passwordDisplay.textContent = password;
+};
